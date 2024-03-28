085f73f7d1c7bf915eca17f709e52f3fbf1d2370
infinitefusion <pkmninfinitefusion@gmail.com>
2024-03-28 19:58:25 -0400

    fixes dex number not updating from previous version

--- Data/Scripts/014_Pokemon/001_Pokemon.rb
+++ Data/Scripts/014_Pokemon/001_Pokemon.rb
@@ -128,10 +128,8 @@ class Pokemon
   end
 
   def species_data
-    if !@species_data || @species != @species_data.species
-      @species_data = GameData::Species.get(@species)
-    end
-    return @species_data #GameData::Species.get(@species)
+    @species_data = GameData::Species.get(@species)
+    return @species_data
   end
 
   #=============================================================================
@@ -231,17 +229,16 @@ class Pokemon
   end
 
   def changeFormSpecies(oldForm, newForm)
-    is_already_old_form = self.isFusionOf(oldForm)  #A 466
-    is_already_new_form = self.isFusionOf(newForm)  #P
-
+    is_already_old_form = self.isFusionOf(oldForm) #A 466
+    is_already_new_form = self.isFusionOf(newForm) #P
 
     #reverse the fusion if it's a meloA and meloP fusion
     # There's probably a smarter way to do this but laziness lol
     if is_already_old_form && is_already_new_form
       if self.species_data.get_body_species() == oldForm
-        changeSpeciesSpecific(self,getFusedPokemonIdFromSymbols(newForm,oldForm))
+        changeSpeciesSpecific(self, getFusedPokemonIdFromSymbols(newForm, oldForm))
       else
-        changeSpeciesSpecific(self,getFusedPokemonIdFromSymbols(oldForm,newForm))
+        changeSpeciesSpecific(self, getFusedPokemonIdFromSymbols(oldForm, newForm))
       end
     else
       changeSpecies(self, oldForm, newForm) if is_already_old_form
@@ -251,11 +248,11 @@ class Pokemon
     calc_stats
   end
 
-  def changeSpecies(pokemon, speciesToReplace,newSpecies)
+  def changeSpecies(pokemon, speciesToReplace, newSpecies)
     if pokemon.isFusion?()
-      replaceFusionSpecies(pokemon,speciesToReplace,newSpecies)
+      replaceFusionSpecies(pokemon, speciesToReplace, newSpecies)
     else
-      changeSpeciesSpecific(pokemon,newSpecies)
+      changeSpeciesSpecific(pokemon, newSpecies)
     end
     $Trainer.pokedex.set_seen(pokemon.species)
     $Trainer.pokedex.set_owned(pokemon.species)
