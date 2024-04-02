99e381f0187d05bd03ea5d0636c5f3860ef3f84f
infinitefusion <pkmninfinitefusion@gmail.com>
2024-04-01 20:25:23 -0400

    fixes some pokemon positionings

--- Data/Scripts/DownloadedSettings.rb
+++ Data/Scripts/DownloadedSettings.rb
@@ -1,6 +1,6 @@
 
 module Settings
-  SHINY_POKEMON_CHANCE = 16
+  SHINY_POKEMON_CHANCE = 32#16
   CREDITS_FILE_URL = "https://gitlab.com/pokemoninfinitefusion/customsprites/-/raw/master/Sprite Credits.csv"
   SPRITES_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/CUSTOM_SPRITES"
   VERSION_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/VERSION"
