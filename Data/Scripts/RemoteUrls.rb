ffd3a95e08a0ddaf8fb57793f3a5174ff73e9533
infinitefusion <pkmninfinitefusion@gmail.com>
2024-09-24 12:00:13 -0400

    Fixes typo in gitlab url

--- Data/Scripts/RemoteUrls.rb
+++ Data/Scripts/RemoteUrls.rb
@@ -7,9 +7,9 @@ module Settings
   CUSTOM_DEX_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/dex.json"
 
   AUTOGEN_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/autogen-fusion-sprites/-/raw/master/Battlers/"
-  CUSTOM_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/customsprites/-/raw/master/CustomBattlers/"
+  CUSTOM_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/customSprites/-/raw/master/CustomBattlers/"
   BASE_POKEMON_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/autogen-fusion-sprites/-/raw/master/Battlers/"
-  BASE_POKEMON_ALT_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/customsprites/-/raw/master/Other/BaseSprites/"
+  BASE_POKEMON_ALT_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/customSprites/-/raw/master/Other/BaseSprites/"
 
   DISCORD_URL = "https://discord.com/invite/infinitefusion"
   WIKI_URL = "https://infinitefusion.fandom.com/"
