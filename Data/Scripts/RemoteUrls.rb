6f69768db6b6e420edc0c934f6055af0904243f1
infinitefusion <pkmninfinitefusion@gmail.com>
2024-09-24 11:56:34 -0400

    Changes gitlab repo url for autogens sprites

--- Data/Scripts/RemoteUrls.rb
+++ Data/Scripts/RemoteUrls.rb
@@ -6,7 +6,7 @@ module Settings
   VERSION_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/VERSION"
   CUSTOM_DEX_FILE_URL = "https://raw.githubusercontent.com/infinitefusion/infinitefusion-e18/main/Data/dex.json"
 
-  AUTOGEN_SPRITES_REPO_URL = "https://gitlab.com/pokemoninfinitefusion/autogen-fusion-sprites/-/raw/master/Battlers/"
+  AUTOGEN_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/autogen-fusion-sprites/-/raw/master/Battlers/"
   CUSTOM_SPRITES_REPO_URL = "https://gitlab.com/infinitefusion2/customsprites/-/raw/master/CustomBattlers/"
   BASE_POKEMON_SPRITES_REPO_URL = "https://gitlab.com/pokemoninfinitefusion/autogen-fusion-sprites/-/raw/master/Battlers/"
   BASE_POKEMON_ALT_SPRITES_REPO_URL = "https://gitlab.com/pokemoninfinitefusion/customsprites/-/raw/master/Other/BaseSprites/"
