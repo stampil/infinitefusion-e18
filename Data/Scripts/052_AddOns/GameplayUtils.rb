5034855f98546b6d3fd2c062ed70e09be87af4aa
infinitefusion <pkmninfinitefusion@gmail.com>
2024-12-21 21:14:44 -0500

    fixes isOutdoor

--- Data/Scripts/052_AddOns/GameplayUtils.rb
+++ Data/Scripts/052_AddOns/GameplayUtils.rb
@@ -1503,7 +1503,7 @@ end
 #Returns if the current map is an outdoor map
 def isOutdoor()
   current_map = $game_map.map_id
-  map_metadata = GameData::MapMetadata.try_get(map_id)
+  map_metadata = GameData::MapMetadata.try_get(current_map)
   return map_metadata && map_metadata.outdoor_map
 end
 
